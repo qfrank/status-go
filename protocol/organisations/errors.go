f18afbfa149b845a42a809cfce4610fe2a2810d5
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2020-10-12 15:18:32 +0200

    Delete chat

--- protocol/organisations/errors.go
+++ protocol/organisations/errors.go
@@ -3,6 +3,7 @@ package organisations
 import "errors"
 
 var ErrChatNotFound = errors.New("chat not found")
+var ErrChatAlreadyExists = errors.New("chat already exists")
 var ErrCantRequestAccess = errors.New("can't request access")
 var ErrInvalidOrganisationDescription = errors.New("invalid organisation description")
 var ErrNotAdmin = errors.New("no admin privileges for this organisation")
